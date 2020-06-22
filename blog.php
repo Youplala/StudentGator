466f099ae4c5c6779611ddb2fddb5aef25d39e38
Youplala <eliebrosset@gmail.com>
2020-06-22 22:48:12 +0200

    blog

---
+++ blog.php
@@ -0,0 +1,232 @@
+<html>
+    <head>
+      <meta charset="utf-8">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+      <link href="https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400;600&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Francois+One&display=swap" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" type="text/css" href="css/blog.css">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <script src="js/script.js"></script>
+    </head>
+  <title>CY TECH - EISTI</title>
+  <header>
+    <?php include 'components/header.php';?>
+    <?php include 'php/dbConfig.php'; ?>
+  </header>
+
+    <body>
+      <div class="main container">
+        <div class="cards_container">
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+          <div class="card">
+            <a href="">
+            <div class="card-image">
+              <img src="images/backgrounds/essec.jpg">
+            </div>
+            <div class="card-content">
+              <p class="black-text">27/01/12</p>
+              <span class="card-title black-text">StudentGator origins</span>
+              <p style="color: grey;">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="article_info">
+                <p style="font-size: 15px;color:#111;">Nicolas Brendon</p>
+              </div>
+            </div>
+            </a>
+          </div>
+
+
+        </div>
+      </div>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
